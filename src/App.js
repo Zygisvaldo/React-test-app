220861668034a963f28a5557eebed31a6129c7b6
Zygisvaldo <zygiodeze@gmail.com>
2023-10-10 13:45:47 +0200

    feat: expense list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import ExpenseItem from "./components/ExpenseItem";
+import ExpenseList from "./components/ExpenseList";
 import { expenses } from "./expenseData";
 
 function App() {
   return (
     <div>
       <h1>Lets get started</h1>
-      <ExpenseItem
-        date={expenses[0].date}
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-      ></ExpenseItem>
+      <ExpenseList expenses={expenses}/>
     </div>
   );
 }
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,19 @@
+import ExpenseItem from "./ExpenseItem";
+import "./ExpenseList.css";
+
+function ExpenseList(props) {
+  return (
+    <div className="expenses">
+      {props.expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.title}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default ExpenseList;
