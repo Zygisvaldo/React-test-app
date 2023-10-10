b75560d4d1f8b6913f252e8392e9e0f6f59011e3
Zygisvaldo <zygiodeze@gmail.com>
2023-10-10 12:11:22 +0200

    feat: styling added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
-function ExpenseItem () {
-  return <h2>Expense item</h2>
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>24-12-2023</div>
+      <div className="expense-item__description">
+        <h2>Tesla Stock Price</h2>
+        <div className="expense-item__price">$420.69</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
