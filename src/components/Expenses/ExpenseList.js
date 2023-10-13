10f5ae41aa644540970e1d62a65393df5669f3e5
Zygisvaldo <zygiodeze@gmail.com>
2023-10-13 17:12:16 +0200

    fix: conditional rendering

--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -42,14 +42,16 @@ const ExpenseList = (props) => {
           onChangeFilter={filterChangeHandler}
         />
         <p>Data for year {filterInfoText} is hidden.</p>
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {filteredExpenses.length === 0 && <p>No expenses found</p>}
+        {filteredExpenses.length > 0 &&
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))}
       </Card>
     </div>
   );
