1ee3e0212d6f48cc7ea56525418fb14f3230f154
Zygisvaldo <zygiodeze@gmail.com>
2023-10-13 17:53:29 +0200

    fix: refactored to return conditional render

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -2,20 +2,22 @@ import "./ExpensesList.css";
 import ExpenseItem from "./ExpenseItem";
 
 const ExpensesList = (props) => {
-  let expensesContent = <p>No expenses found</p>;
-
-  if (props.filteredExpenses.length > 0) {
-    expensesContent = props.filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
+  if (props.filteredExpenses.length === 0) {
+    return <h2 className="expenses-list__fallback">Found no expenses</h2>;
   }
 
-  return <div>{expensesContent}</div>;
+  return (
+    <ul className="expenses-list">
+      {props.filteredExpenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </ul>
+  );
 };
 
 export default ExpensesList;
