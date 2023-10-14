9408badbf14eb4ad9f90fb0a479d66b8baee51f1
Zygisvaldo <zygiodeze@gmail.com>
2023-10-14 11:08:17 +0200

    feat: from open and close

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -11,6 +11,7 @@ const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState(formattedDate);
+  const [showExpensesForm, setShowExpensesForm] = useState(false);
 
   /* const [userInput, setUserInput] = useState({
     enteredTitle: "",
@@ -83,47 +84,68 @@ const ExpenseForm = (props) => {
     setEnteredDate(formattedDate);
   };
 
-  return (
-    <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input
-            type="text"
-            value={enteredTitle}
-            onChange={(event) =>
-              inputChangeHandler("title", event.target.value)
-            }
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input
-            type="number"
-            value={enteredAmount}
-            min="0.01"
-            step="0.01"
-            onChange={(event) =>
-              inputChangeHandler("amount", event.target.value)
-            }
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input
-            type="date"
-            value={enteredDate}
-            min="2023-01-01"
-            max="2027-01-01"
-            onChange={(event) => inputChangeHandler("date", event.target.value)}
-          />
-        </div>
-      </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
-      </div>
-    </form>
+  const cancelFormHandler = () => {
+    setShowExpensesForm(false);
+  };
+
+  const openFormHandler = () => {
+    setShowExpensesForm(true);
+  };
+
+  let fromContent = (
+    <div>
+      <button onClick={openFormHandler}>Open Expense Form</button>
+    </div>
   );
+
+  if (showExpensesForm === true) {
+    fromContent = (
+      <form onSubmit={submitHandler}>
+        <div className="new-expense__controls">
+          <div className="new-expense__control">
+            <label>Title</label>
+            <input
+              type="text"
+              value={enteredTitle}
+              onChange={(event) =>
+                inputChangeHandler("title", event.target.value)
+              }
+            />
+          </div>
+          <div className="new-expense__control">
+            <label>Amount</label>
+            <input
+              type="number"
+              value={enteredAmount}
+              min="0.01"
+              step="0.01"
+              onChange={(event) =>
+                inputChangeHandler("amount", event.target.value)
+              }
+            />
+          </div>
+          <div className="new-expense__control">
+            <label>Date</label>
+            <input
+              type="date"
+              value={enteredDate}
+              min="2023-01-01"
+              max="2027-01-01"
+              onChange={(event) =>
+                inputChangeHandler("date", event.target.value)
+              }
+            />
+          </div>
+        </div>
+        <div className="new-expense__actions">
+          <button onClick={cancelFormHandler}>Cancel</button>
+          <button type="submit">Add Expense</button>
+        </div>
+      </form>
+    );
+  }
+
+  return <div>{fromContent}</div>;
 };
 
 export default ExpenseForm;
